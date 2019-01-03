f00126c82810610f2ff1cba7c6c07ea5cf5dff37
Henri Bergius <henri.bergius@iki.fi>
2019-01-03 19:40:44 +0100

    Port to ES6

---
+++ components/GetUserMedia.js
@@ -0,0 +1,101 @@
+const noflo = require('noflo');
+
+// @runtime noflo-browser
+
+exports.getComponent = () => {
+  const c = new noflo.Component();
+  c.description = 'initialize camera and/or microphone';
+  c.icon = 'video-camera';
+  c.inPorts.add('start', {
+    datatype: 'bang',
+  });
+  c.inPorts.add('stop', {
+    datatype: 'bang',
+  });
+  c.inPorts.add('video', {
+    datatype: 'boolean',
+    control: true,
+    default: true,
+  });
+  c.inPorts.add('audio', {
+    datatype: 'boolean',
+    control: true,
+    default: false,
+  });
+  c.outPorts.add('stream',
+    { datatype: 'object' });
+  c.outPorts.add('url',
+    { datatype: 'string' });
+  c.outPorts.add('error',
+    { datatype: 'object' });
+
+  let stream = null;
+  let ctx = null;
+  const stopStream = () => {
+    if (stream && stream.stop) {
+      stream.stop();
+      stream = null;
+    }
+    if (ctx) {
+      ctx.deactivate();
+      ctx = null;
+    }
+  };
+  c.tearDown = (callback) => {
+    stopStream();
+    callback();
+  };
+
+  c.process((input, output, context) => {
+    if (input.hasData('stop')) {
+      input.getData('stop');
+      stopStream();
+      output.done();
+      return;
+    }
+    if (!input.hasData('start')) { return; }
+    input.getData('start');
+    // Stop previous stream, if any
+    stopStream();
+
+    if (!(typeof navigator !== 'undefined' && navigator !== null ? navigator.mediaDevices : undefined)) {
+      output.done(new Error('navigator.mediaDevices not available.'));
+      return;
+    }
+    if (!(typeof navigator !== 'undefined' && navigator !== null ? navigator.mediaDevices.getUserMedia : undefined)) {
+      // In higher-level graph should provide option to chose image
+      // with file picker here. This will make it work on mobile etc.
+      output.done(new Error('navigator.mediaDevices.getUserMedia not available.'));
+      return;
+    }
+
+    const video = input.hasData('video') ? input.getData('video') : true;
+    const audio = input.hasData('audio') ? input.getData('audio') : false;
+
+    navigator.mediaDevices.getUserMedia({
+      video,
+      audio,
+    }).then((mediaStream) => {
+      stream = mediaStream;
+      ctx = context;
+
+      // Shim
+      if (window.URL == null) {
+        window.URL = (
+          window.webkitURL
+          || window.msURL
+          || window.oURL
+          || null);
+      }
+      if (window.URL.createObjectURL) {
+        output.send({ url: window.URL.createObjectURL(stream) });
+      } else {
+        output.send({ url: stream });
+      }
+      return output.send({ stream });
+    },
+    err => output.done(err));
+  });
+
+  return c;
+};
---
+++ spec/GetUserMedia.js
@@ -0,0 +1,63 @@
+describe('GetUserMedia component', () => {
+  let c = null;
+  let sStart = null;
+  let sUrl = null;
+  let sError = null;
+  let loader = null;
+  before(() => {
+    loader = new noflo.ComponentLoader(baseDir);
+  });
+  beforeEach(function (done) {
+    this.timeout(4000);
+    loader.load('gum/GetUserMedia', (err, instance) => {
+      if (err) {
+        done(err);
+        return;
+      }
+      c = instance;
+      sStart = noflo.internalSocket.createSocket();
+      sUrl = noflo.internalSocket.createSocket();
+      sError = noflo.internalSocket.createSocket();
+      c.inPorts.start.attach(sStart);
+      c.outPorts.url.attach(sUrl);
+      c.outPorts.error.attach(sError);
+      done();
+    });
+  });
+
+  describe('when instantiated', () => {
+    it('should have four input ports', () => {
+      chai.expect(c.inPorts.start).to.be.an('object');
+      chai.expect(c.inPorts.stop).to.be.an('object');
+      chai.expect(c.inPorts.video).to.be.an('object');
+      chai.expect(c.inPorts.audio).to.be.an('object');
+    });
+    it('should have three output ports', () => {
+      chai.expect(c.outPorts.stream).to.be.an('object');
+      chai.expect(c.outPorts.url).to.be.an('object');
+      chai.expect(c.outPorts.error).to.be.an('object');
+    });
+  });
+
+  describe('when started', () => {
+    if ((typeof navigator !== 'undefined' && navigator !== null) && (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia)) {
+      // Can't be tested without interaction
+      it('should make a url on permission', function (done) {
+        this.timeout(10000);
+        sError.once('data', (err) => {
+          chai.expect(err.message).to.equal('Requested device not found');
+          done();
+        });
+        sUrl.once('data', (url) => {
+          chai.expect(url).to.be.a('string');
+          done();
+        });
+        sStart.send(true);
+      });
+    }
+    it('should send an error that gum isn\'t available', () => {
+      sError.once('data', err => chai.expect(err).to.be.an('error'));
+      sStart.send(true);
+    });
+  });
+});
